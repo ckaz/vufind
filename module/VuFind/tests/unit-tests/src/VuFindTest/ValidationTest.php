5d0c7379143f2e301beb766a59b20431eea1f320
Chris Hallberg <crhallberg@gmail.com>
2016-02-08 17:33:27 -0500

    Basic validation test.

---
+++ module/VuFind/tests/unit-tests/src/VuFindTest/ValidationTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * HTML5 Validation Test Class
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Tests
+ * @author   Chris Hallberg <crhallberg@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:unit_tests Wiki
+ */
+namespace VuFindTest;
+use Kevintweber\PhpunitMarkupValidators\Assert\AssertHTML5;
+
+class HtmlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Get base URL of running VuFind instance.
+     *
+     * @param string $path Relative path to add to base URL.
+     *
+     * @return string
+     */
+    protected function validatePath($path = '', $message = null)
+    {
+        $base = getenv('VUFIND_URL');
+        if (empty($base)) {
+            $base = 'http://localhost/vufind';
+        }
+        $html = file_get_contents($base . $path);
+        AssertHTML5::isValidMarkup($html, $message);
+    }
+
+    public function testHTMLValidation()
+    {
+        $this->validatePath('', 'Home page');
+        $this->validatePath('/Search/Results?lookfor=test&type=AllFields', '"test" search');
+        $this->validatePath('/Record/testdeweybrowse', 'Record page');
+    }
+}
